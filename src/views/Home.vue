993a4c782871208cc996bd4792e2ccb76c3259fa
wei.dong <wei.dong120@gmail.com>
2020-01-18 17:22:12 +0800

    18J move left menu to left_menu.json

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,36 +3,23 @@
     <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
     <el-container>
       <el-aside width="200px">
-        <el-menu :default-openeds="['1']">
-          <el-submenu index="1">
-            <template slot="title"><i class="el-icon-circle-plus-outline"></i>Teams</template>
-            <el-menu-item-group>
-              <el-menu-item index="1-1">
-                <router-link to="/user/profile/1">Profile</router-link>
-              </el-menu-item>
-              <el-menu-item index="1-2">
-                <router-link to="/user/list">User List</router-link>
-              </el-menu-item>
-            </el-menu-item-group>
+        <el-menu v-for="(mg,mi) in menuGrp" :key="mg.gname" :default-openeds="mg.expand ? [''+ mi]:[]">
+          <el-submenu :index="''+ mi">
+            <template slot="title"><i class="el-icon-circle-plus-outline"></i>{{ mg.gname }}</template>
+            <el-menu-item v-for="(mt, i) in mg.subitems" :key="mt.id" :index="''+ i">
+              <router-link :to="mt.link">{{ mt.name }}</router-link>
+            </el-menu-item>
           </el-submenu>
-          <el-submenu index="2">
-            <template slot="title"><i class="el-icon-circle-plus-outline"></i>Projects</template>
-            <el-menu-item-group>
-              <el-menu-item index="2-1">Item-1</el-menu-item>
-              <el-menu-item index="2-2">Item-2</el-menu-item>
-            </el-menu-item-group>
-          </el-submenu>          
         </el-menu>
       </el-aside>
 
       <el-container>
-        <el-header style="text-align: right: front-size: 12px">
+        <el-header style="text-align: right; front-size: 12px">
           <el-dropdown>
             <i class="el-icon-setting" style="margin-right: 15px"></i>
             <el-dropdown-menu slot="dropdown">
-              <el-dropdown-item>search</el-dropdown-item>
-              <el-dropdown-item>new</el-dropdown-item>
-              <el-dropdown-item>delete</el-dropdown-item>
+              <el-dropdown-item>Logout</el-dropdown-item>
+              <el-dropdown-item>Setting</el-dropdown-item>
             </el-dropdown-menu>
             <span>{{username}}</span>
           </el-dropdown>
@@ -70,12 +57,20 @@ export default {
   },
   data() {
     return {
-      users: [
-            {name: 'Tom', date: '2018-01-20', address: 'Shanghai'},
-            {name: 'John', date: '2018-01-20', address: 'Shanghai'},
-            {name: 'Sindy', date: '2018-01-20', address: 'Shanghai'},
-          ],
+      menuGrp: [],
     };
+  },
+  created() {
+    this.axios({
+        method: 'get',
+        url: 'http://localhost:8102/left_menu.json',
+      })
+        .then((resp) => {
+          this.menuGrp = resp.data;
+        })
+        .catch((err) => {
+          console.log(err);
+        });
   },
 };
 </script>
